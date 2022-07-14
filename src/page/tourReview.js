bc1aaa53b4d8971435ad5f36ed4a348c8db63e15
Sichu0725 <choihc8511@gmail.com>
2022-07-14 10:46:00 +0900

    이게맞나

--- src/page/tourReview.js
+++ src/page/tourReview.js
@@ -14,16 +14,58 @@ export const TourReview = () => {
 
     }
   }
+  const data = {
+    success : true,
+    data : [
+      {
+        writer : "임태현",
+        title : "hello",
+        content : "내용이 머죠",
+        img : "http://ojsfile.ohmynews.com/STD_IMG_FILE/2021/1023/IE002885862_STD.jpg"
+      },
+      {
+        writer : "주호민",
+        title : "hello",
+        content : "내용이란껀 말이쫑~",
+        img : "https://t1.daumcdn.net/cfile/tistory/220ACD49543246531D"
+      },
+      {
+        writer : "임태현",
+        title : "hello",
+        content : "내용이 머죠",
+        img : "http://ojsfile.ohmynews.com/STD_IMG_FILE/2021/1023/IE002885862_STD.jpg"
+      },
+      {
+        writer : "주호민",
+        title : "hello",
+        content : "내용이란껀 말이쫑~",
+        img : "https://t1.daumcdn.net/cfile/tistory/220ACD49543246531D"
+      },
+      {
+        writer : "임태현",
+        title : "hello",
+        content : "내용이 머죠",
+        img : "http://ojsfile.ohmynews.com/STD_IMG_FILE/2021/1023/IE002885862_STD.jpg"
+      },
+      {
+        writer : "임태현",
+        title : "hello",
+        content : "내용이 머죠?",
+        img : "http://ojsfile.ohmynews.com/STD_IMG_FILE/2021/1023/IE002885862_STD.jpg"
+      },
+      
+    ]
+  }
   return (
     <Layout>
       <img style={review_style.background} src="https://cdn.discordapp.com/attachments/915127755480375306/996950020932784139/d87f23e13081974f.png"></img>
       <div style={review_style.frame}>
-        <ReviewCard writer="임태현" title="hello" content="내용이 머죠" img="http://ojsfile.ohmynews.com/STD_IMG_FILE/2021/1023/IE002885862_STD.jpg" />
-        <ReviewCard writer="주호민" title="hello" content="내용이란껀 말이쫑~" img="https://t1.daumcdn.net/cfile/tistory/220ACD49543246531D" />
-        <ReviewCard writer="임태현" title="hello" content="내용이 머죠" img="http://ojsfile.ohmynews.com/STD_IMG_FILE/2021/1023/IE002885862_STD.jpg" />
-        <ReviewCard writer="주호민" title="hello" content="내용이란껀 말이쫑~" img="https://t1.daumcdn.net/cfile/tistory/220ACD49543246531D" />
-        <ReviewCard writer="임태현" title="hello" content="내용이 머죠" img="http://ojsfile.ohmynews.com/STD_IMG_FILE/2021/1023/IE002885862_STD.jpg" />
-        <ReviewCard writer="임태현" title="hello" content="내용이 머죠" img="http://ojsfile.ohmynews.com/STD_IMG_FILE/2021/1023/IE002885862_STD.jpg" />
+        {data.success ? data.data.map(item => {
+          return (
+            <ReviewCard writer={item.writer} title={item.title} content={item.content} img={item.img} />
+          )
+        }): <></>}
+        
         
       </div>
     </Layout>
